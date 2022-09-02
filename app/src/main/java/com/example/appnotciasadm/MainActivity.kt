36da1b1a834c428ec21898eb231a43a0b67204b0
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-02 15:14:05 -0300

    UI finalizado

--- app/src/main/java/com/example/appnotciasadm/MainActivity.kt
+++ app/src/main/java/com/example/appnotciasadm/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.appnotciasadm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appnotciasadm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
+
+        binding.btnPublicar.setOnClickListener {
+            var título = binding.etTitulo
+            var data = binding.etData
+            var notícia = binding.etNoticia
+            var autor = binding.etAutor
+
+            if (título.text.toString().isEmpty() || data.toString().isEmpty() ||
+                notícia.toString().isEmpty() || autor.toString().isEmpty()) Toast
+                .makeText(this, R.string.campo_vazio, Toast.LENGTH_SHORT).show()
+            else {
+                título.text.clear()
+                notícia.text.clear()
+                data.text.clear()
+                autor.text.clear()
+                Toast.makeText(this, R.string.publicado, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
